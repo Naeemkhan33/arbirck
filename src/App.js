b1219f7a5398749bdf6cbbf415cbdc838261ebf9
najam <dev.najam@gmail.com>
2022-02-06 16:21:03 +0500

    footer done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import NavbarPrimary from "./view/components/NavbarPrimary";
 import RoutesPrimary from "./RoutesPrimary";
 import FooterPrimary from "./view/components/FooterPrimary";
 import Web3 from "web3";
-import Token from "./contracts/Token.json";
-import Vault from "./contracts/Vault.json";
-import ICO from "./contracts/ICO.json";
-import BUSD from "./contracts/BUSD.json";
+// import Token from "./contracts/Token.json";
+// import Vault from "./contracts/Vault.json";
+// import ICO from "./contracts/ICO.json";
+// import BUSD from "./contracts/BUSD.json";
 function App() {
   const [loading, setLoading] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
@@ -22,136 +22,135 @@ function App() {
   const [earnings, setEarnings] = useState("0");
   const [time, setTime] = useState(0);
 
-  async function connectWallet() {
-    try {
-      setLoading(true);
-      if (window.ethereum) {
-        const _web3 = new Web3(window.ethereum);
-        setWeb3(_web3);
-        await window.ethereum.request({ method: "eth_requestAccounts" });
+  // async function connectWallet() {
+  //   try {
+  //     setLoading(true);
+  //     if (window.ethereum) {
+  //       const _web3 = new Web3(window.ethereum);
+  //       setWeb3(_web3);
+  //       await window.ethereum.request({ method: "eth_requestAccounts" });
 
-        const networkId = await _web3.eth.net.getId();
+  //       const networkId = await _web3.eth.net.getId();
 
-        if (networkId !== 56) {
-          alert("please select bsc network");
-          return;
-        }
-        setAddress(window.ethereum.selectedAddress);
+  //       if (networkId !== 56) {
+  //         alert("please select bsc network");
+  //         return;
+  //       }
+  //       setAddress(window.ethereum.selectedAddress);
 
-        let _ico = new _web3.eth.Contract(ICO.abi, ICO.address);
-        setIco(_ico);
+  //       let _ico = new _web3.eth.Contract(ICO.abi, ICO.address);
+  //       setIco(_ico);
 
-        let _token = new _web3.eth.Contract(Token.abi, Token.address);
-        setToken(_token);
-        let brick_balance = await _token.methods
-          .balanceOf(window.ethereum.selectedAddress)
-          .call();
-        
-        setBrickBalance(_web3.utils.fromWei(brick_balance, "ether"));
+  //       let _token = new _web3.eth.Contract(Token.abi, Token.address);
+  //       setToken(_token);
+  //       let brick_balance = await _token.methods
+  //         .balanceOf(window.ethereum.selectedAddress)
+  //         .call();
 
-        let _vault = new _web3.eth.Contract(Vault.abi, Vault.address);
-        setVault(_vault);
+  //       setBrickBalance(_web3.utils.fromWei(brick_balance, "ether"));
 
-        let b = await _vault.methods
-          .balanceOf(window.ethereum.selectedAddress)
-          .call();
-        let e = await _vault.methods
-          .earned(window.ethereum.selectedAddress)
-          .call();
-        let c = await _vault.methods
-          .newDeposit(window.ethereum.selectedAddress)
-          .call();
-        let d = await _vault.methods.hodlDuration().call();
-        
-        setVaultBalance(_web3.utils.fromWei(b, "ether"));
-        setEarnings(_web3.utils.fromWei(e, "ether"));
+  //       let _vault = new _web3.eth.Contract(Vault.abi, Vault.address);
+  //       setVault(_vault);
 
+  //       let b = await _vault.methods
+  //         .balanceOf(window.ethereum.selectedAddress)
+  //         .call();
+  //       let e = await _vault.methods
+  //         .earned(window.ethereum.selectedAddress)
+  //         .call();
+  //       let c = await _vault.methods
+  //         .newDeposit(window.ethereum.selectedAddress)
+  //         .call();
+  //       let d = await _vault.methods.hodlDuration().call();
 
-        setTime(parseInt(c) + parseInt(d));
-        console.log(parseInt(c) );
-        console.log( parseInt(d));
+  //       setVaultBalance(_web3.utils.fromWei(b, "ether"));
+  //       setEarnings(_web3.utils.fromWei(e, "ether"));
 
-        let _busd = new _web3.eth.Contract(BUSD.abi, BUSD.address);
-        setBusd(_busd);
-        let busd_balance = await _busd.methods
-          .balanceOf(window.ethereum.selectedAddress)
-          .call();
-        setBusdBalance(_web3.utils.fromWei(busd_balance, "ether"));
-        setLoading(false);
-      } else {
-        alert("please install metamask");
-        return;
-      }
-    } catch (e) {
-      alert("Error:", e);
-      return;
-    }
-  }
+  //       setTime(parseInt(c) + parseInt(d));
+  //       console.log(parseInt(c) );
+  //       console.log( parseInt(d));
 
-  async function buyToken(amount) {
-    try {
-      let a = await web3.utils.toWei(amount);
-      let allowance = await busd.methods.allowance(window.ethereum.selectedAddress,ICO.address).call();
-    
-      console.log(`allowance`, allowance)
-      if(allowance === "0"){
-        await busd.methods
-        .approve(ICO.address, web3.utils.toWei("4850999393"))
-        .send({ from: window.ethereum.selectedAddress,gasLimit:"210000" })
-        .on("transactionHash", async () => {
-          await ico.methods
-            .buyTokens(window.ethereum.selectedAddress, a)
-            .send({ from: window.ethereum.selectedAddress });
-        })
-        .on("error", async (e) => {
-          console.log("Error", e);
-          return;
-        });
-      }else{
-        await ico.methods
-        .buyTokens(window.ethereum.selectedAddress, a)
-        .send({ from: window.ethereum.selectedAddress });
-      }
-    
-    } catch (e) {
-      console.log(e.message);
-      return;
-    }
-  }
-  async function depositToVault(amount) {
-    try {
-      let a = await web3.utils.toWei(amount);
-      let allowance = await token.methods.allowance(window.ethereum.selectedAddress,Vault.address).call();
-      if(allowance === "0"){
-      await token.methods
-      .approve(Vault.address, web3.utils.toWei("100004"))
-      .send({ from: window.ethereum.selectedAddress,gasLimit:"210000" })
-      .on("transactionHash", async () => {
-      await vault.methods
-        .stake(a)
-        .send({ from: window.ethereum.selectedAddress, gasLimit:"210000"})
-      })
-      .on("error", async (e) => {
-          console.log("Error", e);
-          return;
-        });
-      }else{
-        await vault.methods
-        .stake(a)
-        .send({ from: window.ethereum.selectedAddress, gasLimit:"210000"})
-      }
-    } catch (e) {
-      console.log(e.message);
-      return;
-    }
-  }
+  //       let _busd = new _web3.eth.Contract(BUSD.abi, BUSD.address);
+  //       setBusd(_busd);
+  //       let busd_balance = await _busd.methods
+  //         .balanceOf(window.ethereum.selectedAddress)
+  //         .call();
+  //       setBusdBalance(_web3.utils.fromWei(busd_balance, "ether"));
+  //       setLoading(false);
+  //     } else {
+  //       alert("please install metamask");
+  //       return;
+  //     }
+  //   } catch (e) {
+  //     alert("Error:", e);
+  //     return;
+  //   }
+  // }
+
+  // async function buyToken(amount) {
+  //   try {
+  //     let a = await web3.utils.toWei(amount);
+  //     let allowance = await busd.methods.allowance(window.ethereum.selectedAddress,ICO.address).call();
+
+  //     console.log(`allowance`, allowance)
+  //     if(allowance === "0"){
+  //       await busd.methods
+  //       .approve(ICO.address, web3.utils.toWei("4850999393"))
+  //       .send({ from: window.ethereum.selectedAddress,gasLimit:"210000" })
+  //       .on("transactionHash", async () => {
+  //         await ico.methods
+  //           .buyTokens(window.ethereum.selectedAddress, a)
+  //           .send({ from: window.ethereum.selectedAddress });
+  //       })
+  //       .on("error", async (e) => {
+  //         console.log("Error", e);
+  //         return;
+  //       });
+  //     }else{
+  //       await ico.methods
+  //       .buyTokens(window.ethereum.selectedAddress, a)
+  //       .send({ from: window.ethereum.selectedAddress });
+  //     }
+
+  //   } catch (e) {
+  //     console.log(e.message);
+  //     return;
+  //   }
+  // }
+  // async function depositToVault(amount) {
+  //   try {
+  //     let a = await web3.utils.toWei(amount);
+  //     let allowance = await token.methods.allowance(window.ethereum.selectedAddress,Vault.address).call();
+  //     if(allowance === "0"){
+  //     await token.methods
+  //     .approve(Vault.address, web3.utils.toWei("100004"))
+  //     .send({ from: window.ethereum.selectedAddress,gasLimit:"210000" })
+  //     .on("transactionHash", async () => {
+  //     await vault.methods
+  //       .stake(a)
+  //       .send({ from: window.ethereum.selectedAddress, gasLimit:"210000"})
+  //     })
+  //     .on("error", async (e) => {
+  //         console.log("Error", e);
+  //         return;
+  //       });
+  //     }else{
+  //       await vault.methods
+  //       .stake(a)
+  //       .send({ from: window.ethereum.selectedAddress, gasLimit:"210000"})
+  //     }
+  //   } catch (e) {
+  //     console.log(e.message);
+  //     return;
+  //   }
+  // }
 
   async function withdraw(amount) {
     try {
       let a = await web3.utils.toWei(amount);
       await vault.methods
         .withdraw(a)
-        .send({ from: window.ethereum.selectedAddress,gasLimit:"210000" })
+        .send({ from: window.ethereum.selectedAddress, gasLimit: "210000" })
         .on("error", async (e) => {
           console.log("Error", e);
           return;
@@ -166,7 +165,7 @@ function App() {
     try {
       await vault.methods
         .getReward()
-        .send({ from: window.ethereum.selectedAddress ,gasLimit:"210000"})
+        .send({ from: window.ethereum.selectedAddress, gasLimit: "210000" })
         .on("error", async (e) => {
           console.log("Error", e);
           return;
@@ -193,15 +192,15 @@ function App() {
             />
           </button>
           <NavbarPrimary
-            connectWallet={connectWallet}
+            // connectWallet={connectWallet}
             address={address}
-            buyToken={buyToken}
+            // buyToken={buyToken}
           />
           <RoutesPrimary
             brickBalance={brickBalance}
             busdBalance={busdBalance}
-            buyToken={buyToken}
-            depositToVault={depositToVault}
+            // buyToken={buyToken}
+            // depositToVault={depositToVault}
             withdraw={withdraw}
             getReward={getReward}
             vaultBalance={vaultBalance}
--- src/view/components/MonochromePie.jsx
+++ src/view/components/MonochromePie.jsx
@@ -1,62 +1,49 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+import React from "react";
+import ReactApexChart from "react-apexcharts";
 
-class MonochromePie extends React.Component {
-  constructor(props) {
-    super(props);
+function MonochromePie({ places, series, colors, pieHeading, pieValue }) {
+  const options = {
+    chart: {
+      width: "100%",
+      type: "pie",
+    },
 
-    this.state = {
-      series: [25, 15, 44, 55, 41],
-      options: {
-        chart: {
-          width: '100%',
-          type: 'pie',
-        },
+    colors: colors,
 
-        colors: ['#92BDC3', '#6F93A3', '#536A80', '#3C435C', '#261F36'],
-
-        labels: ['London', 'Liverpool', 'Manchester', 'Birmingham', 'Leeds'],
-        theme: {
-          monochrome: {
-            enabled: false,
-          },
-        },
-        plotOptions: {
-          pie: {
-            dataLabels: {
-              offset: -5,
-            },
-          },
-        },
-
+    labels: places,
+    theme: {
+      monochrome: {
+        enabled: false,
+      },
+    },
+    plotOptions: {
+      pie: {
         dataLabels: {
-          formatter(val, opts) {
-            const name = opts.w.globals.labels[opts.seriesIndex];
-            return [name, val.toFixed(1) + '%'];
-          },
-        },
-        legend: {
-          show: false,
+          offset: -5,
         },
       },
-    };
-  }
+    },
 
-  render() {
-    return (
-      <div id='chart'>
-        <ReactApexChart
-          options={this.state.options}
-          series={this.state.series}
-          type='pie'
-        />
-        <div className='text-center'>
-          <h6 className='fw-bold mb-0'>112</h6>
-          <h6 className='fw-normal mb-0'>Properties Under Management</h6>
-        </div>
+    dataLabels: {
+      formatter(val, opts) {
+        const name = opts.w.globals.labels[opts.seriesIndex];
+        return [name, val.toFixed(1) + "%"];
+      },
+    },
+    legend: {
+      show: false,
+    },
+  };
+
+  return (
+    <div id="chart">
+      <ReactApexChart options={options} series={series} type="pie" />
+      <div className="text-center">
+        <h6 className="fw-bold mb-0">{pieValue}</h6>
+        <h6 className="fw-normal mb-0">{pieHeading}</h6>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MonochromePie;
